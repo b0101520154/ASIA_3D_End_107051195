b4788f8c193b8d0c5d76d72b3da9870c7ef1377f
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2021-01-08 02:57:33 +0800

    NPC對話

---
+++ 3DEnd/Assets/Scripts/NPC.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+    public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirsrDialog,Missioning,Finish
+    }
+
+    public NPCState state = NPCState.FirsrDialog;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家軍人")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "玩家軍人")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Dialog()
+    {
+        dialog.SetActive(true);
+        textContent.text = "";
+
+        textName.text = name;
+
+        string dialogString = data.dialogA;
+
+        switch (state)
+        {
+            case NPCState.FirsrDialog:
+                dialogString = data.dialogA;
+                break;
+            case NPCState.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case NPCState.Finish:
+                dialogString = data.dialogC;
+                break;
+        }
+
+        for (int i = 0; i < dialogString.Length; i++)
+        {
+            textContent.text += dialogString[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+}
---
+++ 3DEnd/Assets/Scripts/NPCData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "NPC資料",menuName = "Vinenia/NPC資料")]
+
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1,3)]
+    public string dialogA;
+    [Header("第二段對話"), TextArea(1,3)]
+    public string dialogB;
+    [Header("第三段對話"), TextArea(1,3)]
+    public string dialogC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+}
